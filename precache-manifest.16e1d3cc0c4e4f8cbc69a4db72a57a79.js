7d01d2779e00cfcc2704a65e166bbdf13f704d36
l1grn <sidorov.gennadi@gmail.com>
2020-03-26 18:50:52 +0300

    Updates

---
+++ precache-manifest.16e1d3cc0c4e4f8cbc69a4db72a57a79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5426d52bf07857451cc31da6e2098596",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80ee9938d6818586ddd3",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "34d83463c79a498f6161",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "80ee9938d6818586ddd3",
+    "url": "./static/js/2.f1faac46.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.f1faac46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d83463c79a498f6161",
+    "url": "./static/js/main.551f391a.chunk.js"
+  },
+  {
+    "revision": "628eaaaaf82a98dd9226",
+    "url": "./static/js/runtime-main.a708e874.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cf89dad57b86c71c4530c14ed6e05aa.js"
+  "./precache-manifest.16e1d3cc0c4e4f8cbc69a4db72a57a79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
